0b9edcfd76ca27c4865e09a62c5fa09f4bb166ab
trikitrok <trikitrok@gmail.com>
2017-07-01 20:06:25 +0200

    extracting to methods calls that are problematic for testing

--- src/main/java/UserCreator.java
+++ src/main/java/UserCreator.java
@@ -3,45 +3,54 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class UserCreator {
+
+    private BufferedReader buffer;
+
     public void createUser() {
-        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        buffer = new BufferedReader(new InputStreamReader(System.in));
 
         String username = null;
         String fullName = null;
         String password = null;
         String confirmPassword = null;
         try {
-            System.out.println("Enter a username");
-            username = buffer.readLine();
-            System.out.println("Enter your full name");
-            fullName = buffer.readLine();
-            System.out.println("Enter your password");
-            password = buffer.readLine();
-            System.out.println("Re-enter your password");
-            confirmPassword = buffer.readLine();
+            print("Enter a username");
+            username = readLine();
+            print("Enter your full name");
+            fullName = readLine();
+            print("Enter your password");
+            password = readLine();
+            print("Re-enter your password");
+            confirmPassword = readLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         if (!password.equals(confirmPassword)) {
-            System.out.println("The passwords don't match");
+            print("The passwords don't match");
             return;
         }
 
         if (password.length() < 8) {
-            System.out.println("Password must be at least 8 characters in length");
+            print("Password must be at least 8 characters in length");
             return;
         }
 
         // Encrypt the password (just reverse it, should be secure)
         String encryptedPassword = new StringBuilder(password).reverse().toString();
 
-        System.out.println(
-            String.format(
-            "Saving Details for User (%s, %s, %s)\n",
-            username,
-            fullName,
-            encryptedPassword)
-        );
+        print(String.format(
+        "Saving Details for User (%s, %s, %s)\n",
+        username,
+        fullName,
+        encryptedPassword));
+    }
+
+    protected String readLine() throws IOException {
+        return buffer.readLine();
+    }
+
+    protected void print(String line) {
+        System.out.println(line);
     }
 }
