675922b56c5cdc7f638aa1c91487ffde2155e0f0
trikitrok <trikitrok@gmail.com>
2017-07-01 21:17:56 +0200

    moving ConsoleUserDataRetrieval to its own file

--- src/main/java/UserCreator.java
+++ src/main/java/UserCreator.java
@@ -30,24 +30,4 @@ class UserCreator {
                 encryptedPassword));
     }
 
-    private class ConsoleUserDataRetrieval {
-        private final Console console;
-
-        public ConsoleUserDataRetrieval(Console console) {
-            this.console = console;
-        }
-
-        public UserData invoke() {
-            console.printLine("Enter a username");
-            String username = console.readLine();
-            console.printLine("Enter your full name");
-            String fullName = console.readLine();
-            console.printLine("Enter your password");
-            String password = console.readLine();
-            console.printLine("Re-enter your password");
-            String confirmPassword = console.readLine();
-
-            return new UserData(username, fullName, password, confirmPassword);
-        }
-    }
 }
